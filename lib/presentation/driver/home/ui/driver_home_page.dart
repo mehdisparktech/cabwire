1419e1ab6e9ec0cbc97a74339e7ae6f38573e2de
ird-mehdi <mehdidev.ird@gmail.com>
2025-05-17 01:22:36 +0600

    Remove unused import for common_image in DriverHomePage to enhance code clarity and maintainability.

--- lib/presentation/driver/home/ui/driver_home_page.dart
+++ lib/presentation/driver/home/ui/driver_home_page.dart
@@ -1,5 +1,4 @@
 import 'package:cabwire/core/utility/utility.dart';
-import 'package:cabwire/presentation/common/components/common_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
