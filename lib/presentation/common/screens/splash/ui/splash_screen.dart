9dd7371ece85f5e0b66fdaab82a3d872574df394
ird-mehdi <mehdidev.ird@gmail.com>
2025-05-20 10:50:45 +0600

    splase_screen

--- lib/presentation/common/screens/splash/ui/splash_screen.dart
+++ lib/presentation/common/screens/splash/ui/splash_screen.dart
@@ -1,25 +1,25 @@
-import 'package:cabwire/core/config/app_assets.dart';
-import 'package:cabwire/presentation/common/screens/splash/ui/welcome_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+// import 'package:cabwire/core/config/app_assets.dart';
+// import 'package:cabwire/presentation/common/screens/splash/ui/welcome_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 3), () {
-      Get.offAll(() => WelcomeScreen()); // Navigate to WelcomeScreen
-    });
+//   @override
+//   Widget build(BuildContext context) {
+//     Future.delayed(const Duration(seconds: 3), () {
+//       Get.offAll(() => WelcomeScreen()); // Navigate to WelcomeScreen
+//     });
 
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset(AppAssets.icSplashScreen, fit: BoxFit.cover),
-          Center(child: Image.asset(AppAssets.icSplashScreenLogo, width: 200)),
-        ],
-      ),
-    );
-  }
-}
+//     return Scaffold(
+//       body: Stack(
+//         fit: StackFit.expand,
+//         children: [
+//           Image.asset(AppAssets.icSplashScreen, fit: BoxFit.cover),
+//           Center(child: Image.asset(AppAssets.icSplashScreenLogo, width: 200)),
+//         ],
+//       ),
+//     );
+//   }
+// }
