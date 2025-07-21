c9f84c580d29052f2bbab450468547b70067deb5
mehdisparktech <mehdisparktech@gmail.com>
2025-07-21 17:55:20 +0600

    Update dependencies and refactor Android/iOS configurations
    - Added 'rename' package dependency in pubspec.yaml for enhanced functionality.
    - Updated Android application ID to 'com.cabwire.cabwire' for consistency across platforms.
    - Refactored build.gradle.kts to load keystore properties for improved security in signing configurations.
    - Adjusted iOS project settings to reflect the new application ID, ensuring alignment with Android configurations.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,20 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 
 android {
     namespace = "com.example.cabwire"
@@ -22,23 +33,21 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cabwire"
+        applicationId = "com.cabwire.cabwire"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21 // Update to 21 as required by NDK
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        
         // Enable multidex
         multiDexEnabled = true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("release")
             signingConfig = signingConfigs.getByName("debug")
         }
     }
